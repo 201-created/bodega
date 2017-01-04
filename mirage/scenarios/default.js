7204ecc004d34ff7e85b30a1337642e183e2bc70
Cory <cory.forsyth@gmail.com>
2017-01-04 16:56:27 -0500

    Add more accurate fixture sticker data

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,3 +1,12 @@
 export default function(server) {
-  return server.createList('item', 5);
+  // TODO(Jorge): Update stickers with actual assets
+  let stickerData = [
+    {name: 'ghost', url: 'sticker-ghost.png'},
+    {name: 'fingers', url: 'sticker-fingers.png'},
+    {name: 'prince', url: 'sticker-prince.png'}
+  ];
+
+  stickerData.forEach(({name, url}) => {
+    server.create('item', {name, url});
+  });
 }
