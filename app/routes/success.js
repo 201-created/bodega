b09de76f52e18b9482544a8156f63ea4a5ce0e2f
Isaac Ezer <isaac.ezer@gmail.com>
2017-01-09 16:31:22 -0500

    add fake charge for styling success route

--- app/routes/success.js
+++ app/routes/success.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
+// For testing visit /success/test
 export default Ember.Route.extend({
+  model(params) {
+    if(params.id === 'test') {
+      return fakeRecord(this.store);
+    }
+
+    // Backend for fetching charges not implemented
+    // Currently we always pass in a model after a purchase is complete
+    // see component:apple-pay-button
+  }
 });
+
+function fakeRecord(store) {
+  let item = store.peekRecord('item', '1');
+  return store.createRecord('charge', {
+    item,
+    price: item.price,
+    id: '12345',
+    token: '12345ABCDE',
+    shippingContact: {
+      givenName: 'Bob',
+      familyName: 'Jones',
+      emailAddress: 'bob@201-created.com',
+      addressLines: [
+        '240 Kent Ave.',
+        'Suite 7'
+      ],
+      locality: 'Brooklyn',
+      administrativeArea: 'New York',
+      postalCode: '11234'
+    }
+  });
+
+}
