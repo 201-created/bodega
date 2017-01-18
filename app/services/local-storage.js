d24d7e0a1833f8bc2905d2db9524ba38369cb8fb
Cory <cory.forsyth@gmail.com>
2017-01-18 16:28:57 -0500

    Detect if window.localStorage and getItem/setItem are available
    Fixes Fastboot errors on initial boot

--- app/services/local-storage.js
+++ app/services/local-storage.js
@@ -2,14 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   unknownProperty(key) {
-    if (typeof window.localStorage !== undefined) {
+    if (window.localStorage && window.localStorage.getItem) {
       return window.localStorage.getItem(key);
     }
   },
 
   setUnknownProperty(key, value) {
-    if (typeof window.localStorage !== undefined) {
-      window.localStorage.setItem(key, value);
+    if (window.localStorage && window.localStorage.setItem) {
+      return window.localStorage.setItem(key, value);
     }
   }
 });
